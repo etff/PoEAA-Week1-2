3218c0dc9956183ca78ed350ff2fcbc07c9068a5
etff <wish2rich@gmail.com>
2024-02-28 21:13:41 +0900

    refactor: Quantity 값 객체로 변경

---
+++ src/test/java/org/example/layered/domain/QuantityTest.java
@@ -0,0 +1,65 @@
+package org.example.layered.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityTest {
+
+    @Test
+    void constructor_setsValueCorrectly() {
+        // Arrange
+        Integer value = 1;
+
+        // Act
+        Quantity quantity = new Quantity(value);
+
+        // Assert
+        assertEquals(value, quantity.getValue());
+    }
+
+    @Test
+    void constructor_throwsExceptionForNegativeValue() {
+        // Arrange
+        Integer negativeValue = -1;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> new Quantity(negativeValue));
+    }
+
+    @Test
+    void add_increasesQuantityByGivenValue() {
+        // Arrange
+        Quantity quantity = new Quantity(1);
+        int additionalValue = 2;
+
+        // Act
+        Quantity newQuantity = quantity.add(additionalValue);
+
+        // Assert
+        assertEquals(3, newQuantity.getValue());
+    }
+
+    @Test
+    void subtract_decreasesQuantityByGivenValue() {
+        // Arrange
+        Quantity quantity = new Quantity(3);
+        int subtractValue = 2;
+
+        // Act
+        Quantity newQuantity = quantity.subtract(subtractValue);
+
+        // Assert
+        assertEquals(1, newQuantity.getValue());
+    }
+
+    @Test
+    void subtract_throwsExceptionForValueGreaterThanQuantity() {
+        // Arrange
+        Quantity quantity = new Quantity(1);
+        int subtractValue = 2;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> quantity.subtract(subtractValue));
+    }
+}
