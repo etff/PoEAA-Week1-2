9bd01dee6a06e34f19a95773d98282e9ba3c1ebe
etff <wish2rich@gmail.com>
2024-02-28 21:03:23 +0900

    refactor: 서비스에서 상품을 조회하여 검증하도록 변경

--- src/main/java/org/example/layered/application/AddProductToCartService.java
+++ src/main/java/org/example/layered/application/AddProductToCartService.java
@@ -2,22 +2,30 @@ package org.example.layered.application;
 
 import org.example.layered.domain.Cart;
 import org.example.layered.domain.LineItem;
+import org.example.layered.domain.Product;
 import org.example.layered.infra.CartRepository;
+import org.example.layered.infra.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AddProductToCartService {
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
 
-    public AddProductToCartService(CartRepository cartRepository) {
+    public AddProductToCartService(CartRepository cartRepository, ProductRepository productRepository) {
         this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
     }
 
     @Transactional
     public Long addProduct(Long productId, Long optionId, Integer quantity) {
         Cart cart = new Cart();
-        LineItem lineItem = new LineItem(productId, optionId, quantity);
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("product not found"));
+
+        LineItem lineItem = new LineItem(product, optionId, quantity);
         cart.addProduct(lineItem);
 
         Cart savedCart = cartRepository.save(cart);
@@ -28,7 +36,11 @@ public class AddProductToCartService {
     public Long updateProduct(Long cartId, Long productId, Long optionId, Integer quantity) {
         Cart cart = cartRepository.findById(cartId)
                 .orElseThrow(() -> new IllegalArgumentException("cart not found"));
-        LineItem lineItem = new LineItem(productId, optionId, quantity);
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("product not found"));
+
+        LineItem lineItem = new LineItem(product, optionId, quantity);
 
         cart.addProduct(lineItem);
 
