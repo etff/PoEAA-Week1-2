70740bbe3a648495a03337760107bb3793e3fa30
etff <wish2rich@gmail.com>
2024-03-01 11:38:15 +0900

    refactor: VO 객체 record 로 수정

--- src/main/java/org/example/layered/domain/Quantity.java
+++ src/main/java/org/example/layered/domain/Quantity.java
@@ -2,43 +2,20 @@ package org.example.layered.domain;
 
 import jakarta.persistence.Embeddable;
 
-import java.util.Objects;
-
 @Embeddable
-public class Quantity {
-    private int value;
+public record Quantity(int value) {
 
-    protected Quantity() {
-    }
-
-    public Quantity(int value) {
+    public Quantity {
         if (value <= 0) {
             throw new IllegalArgumentException("quantity must be greater than 0");
         }
-        this.value = value;
     }
 
-    public Quantity add(int quantity) {
-        return new Quantity(this.value + quantity);
+    public Quantity add( int addition) {
+        return new Quantity(value + addition);
     }
 
-    public Quantity subtract(int quantity) {
-        return new Quantity(this.value - quantity);
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Quantity quantity)) return false;
-        return getValue() == quantity.getValue();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getValue());
+    public Quantity subtract(int subtraction) {
+        return new Quantity(value - subtraction);
     }
 }
