a571b145f876ff1e43191cc433eb0e3664a67bdb
etff <wish2rich@gmail.com>
2024-03-09 19:31:50 +0900

    refactor: 도메인 이멘트 변경

--- src/main/java/org/example/layered/application/AddCartEventListener.java
+++ src/main/java/org/example/layered/application/AddCartEventListener.java
@@ -4,18 +4,18 @@ import org.example.layered.domain.Cart;
 import org.example.layered.domain.CartPublishedEvent;
 import org.example.layered.infra.CartRedisRepository;
 import org.example.layered.query.dto.CartView;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AddCartEventListener {
+public class AddCartEventListener implements ApplicationListener<CartPublishedEvent> {
     private final CartRedisRepository cartRedisRepository;
 
     public AddCartEventListener(CartRedisRepository cartRedisRepository) {
         this.cartRedisRepository = cartRedisRepository;
     }
 
-    @EventListener
+    @Override
     public void onApplicationEvent(CartPublishedEvent event) {
         Cart cart = event.getCart();
         cartRedisRepository.save(new CartView(cart));
